abc82884cc7b92ed2f4c48002cdee5e0ae1c4081
Luana Paes <luanafranca598@gmail.com>
2025-06-27 12:13:39 -0300

    feat: atualiza HomeComponent para renderizar os livros dinamicamente do array

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 import { BookCardComponent } from '../../shared/components/book-card/book-card.component';
+import { CustomBook } from '../../shared/interfaces/custom-book';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,22 @@ import { BookCardComponent } from '../../shared/components/book-card/book-card.c
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  bookArray: CustomBook[] = [
+    {
+      name: "Biblioteca da Meia-Noite",
+      description: "Livro de ficção científica que traz valiosas lições sobre escolhas.",
+      image_url: "https://m.media-amazon.com/images/I/81iqH8dpjuL.jpg"
+    },
+    {
+      name: "Bibliotecaria Louca",
+      description: "Livro de ficção científica que traz valiosas lições sobre escolhas.",
+      image_url: "https://m.media-amazon.com/images/I/81iqH8dpjuL.jpg"
+    },
+    {
+      name: "Biblioteca do Meio-Dia",
+      description: "Livro de ficção científica que traz valiosas lições sobre escolhas.",
+      image_url: "https://m.media-amazon.com/images/I/81iqH8dpjuL.jpg"
+    }
+  ]
 
 }
