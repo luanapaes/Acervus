aebedba185076dc1a2f1fe1a418f2e66f8eb38d6
Luana Paes <luanafranca598@gmail.com>
2025-06-27 18:36:54 -0300

    feat: implementa LoginComponent com validação do formulário e estilos

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  
+  loginForm = new FormGroup({
+    email: new FormControl("", [
+      Validators.required,
+      Validators.email
+    ]), 
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  });
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log("Válido")
+    } else{
+      console.log("Não válido")
+    }
+  }
+
+}
