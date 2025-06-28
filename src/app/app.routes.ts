88fb7b15b7abefd709fb17497e8e3c5fb74ddc96
Luana Paes <luanafranca598@gmail.com>
2025-06-27 23:06:43 -0300

    feat: adiciona rota para o UserDashboardComponent no sistema de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent
+    }, 
+    {
+        path: 'userdashboard',
+        component: UserDashboardComponent
     }
 ];
