4ea67fc9c29377247166dc49560257496878b39e
Luana Paes <luanafranca598@gmail.com>
2025-07-02 11:49:30 -0300

    feat: adiciona componente de sidebar com navegação e estilos

---
+++ src/app/pages/adm-dashboard/left-sidebar/left-sidebar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-left-sidebar',
+  standalone: true,
+  imports: [MatSidenavModule, MatButtonModule, RouterLink, RouterOutlet],
+  templateUrl: './left-sidebar.component.html',
+  styleUrl: './left-sidebar.component.scss'
+})
+export class LeftSidebarComponent {
+  isOpen = true;
+
+  toggleSidenav() {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeSidenav() {
+    this.isOpen = false;
+  }
+}
