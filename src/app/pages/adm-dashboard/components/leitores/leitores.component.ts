ee54940f0030f868fe2f496c7274e80bb8d3b557
Luana Paes <luanafranca598@gmail.com>
2025-07-10 16:37:00 -0300

    feat: cria LeitoresComponents

---
+++ src/app/pages/adm-dashboard/components/leitores/leitores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leitores',
+  standalone: true,
+  imports: [],
+  templateUrl: './leitores.component.html',
+  styleUrl: './leitores.component.scss'
+})
+export class LeitoresComponent {
+
+}
