f4b1e0a3fab014d1b7f99fda3304b97febbffa97
Luana Paes <luanafranca598@gmail.com>
2025-07-10 15:34:55 -0300

    refactor: remove BorrowedBook e Reader interfaces

--- src/app/shared/interfaces/borrowed-book.ts
+++
@@ -1,6 +0,0 @@
-export interface BorrowedBook {
-    book_name: string;
-    book_description: string;
-    loan_date: string;
-    loan_qtd: number;
-}
--- src/app/shared/interfaces/reader.ts
+++
@@ -1,8 +0,0 @@
-import { BorrowedBook } from "./borrowed-book";
-
-export interface Reader {
-    fullname: string;
-    email: string;
-    phone_number: string;
-    borrowed_book: BorrowedBook[];
-}
