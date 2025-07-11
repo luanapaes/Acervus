a20a1e02109f3c4718b6c21a8b503c331d9ec56b
Luana Paes <luanafranca598@gmail.com>
2025-07-11 13:05:47 -0300

    feat: implementa snackbar para feedback de criação de evento

--- src/app/pages/adm-dashboard/components/create-evento-dialog/create-evento-dialog.component.ts
+++ src/app/pages/adm-dashboard/components/create-evento-dialog/create-evento-dialog.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Evento } from '../../../../shared/interfaces/evento';
 import { EventoService } from '../../../../shared/services/evento.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CreateEventoSnackbarComponent } from '../../../../shared/components/create-evento-snackbar/create-evento-snackbar.component';
 
 @Component({
   selector: 'app-create-evento-dialog',
@@ -13,12 +15,24 @@ import { EventoService } from '../../../../shared/services/evento.service';
   styleUrl: './create-evento-dialog.component.scss'
 })
 export class CreateEventoDialogComponent {
-  eventoService = inject(EventoService)
+  eventoService = inject(EventoService);
+
+  durationInSeconds = 5;
+
+  constructor(private _snackBar: MatSnackBar) { }
+
+  
   eventoForm = new FormGroup({
     evento_title: new FormControl('', Validators.required),
     evento_description: new FormControl('', Validators.required)
   });
-
+  
+  openSnackBar(message: string) {
+    this._snackBar.openFromComponent(CreateEventoSnackbarComponent, {
+      duration: this.durationInSeconds * 1000,
+      data: {message}
+    });
+  }
 
   onSubmit() {
     if (this.eventoForm.valid) {
@@ -27,8 +41,9 @@ export class CreateEventoDialogComponent {
         description: this.eventoForm.value.evento_description as string
       }
       this.eventoService.create(evento);
+      this.openSnackBar("Evento cadastrado com sucesso!")
     } else{
-      console.log("informe o titulo/descrição do evento");
+      this.openSnackBar("Por favor, informe o título e descrição do evento.")
     }
   }
 
