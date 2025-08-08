ea9eb722fb2add989cca5bd385e6099391c0725d
Luana Paes <luanafranca598@gmail.com>
2025-08-08 11:58:06 -0300

    feat: implementa serviço EmprestimoVencido para cálculo de datas de devolução e dias restantes

---
+++ src/app/shared/services/emprestimo-vencido.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmprestimoVencidoService {
+  taxa: number = 1.30;
+
+  constructor() { }
+
+  calcularDataDevolucao(dataInicial: Date) {
+    return new Date(dataInicial.getTime() + 7 * 24 * 60 * 60 * 1000);
+  }
+
+  calcularDiasRestantes(dataInicial: Date) {
+    const hoje = new Date();
+    const dataDevolucao = this.calcularDataDevolucao(dataInicial);
+    const diffMs = dataDevolucao.getTime() - hoje.getTime();
+    return Math.ceil(diffMs / (1000 * 60 * 60 * 24));
+  }
+}
