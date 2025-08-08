402879fdf3adec68791451874c3cbba91ce74070
Luana Paes <luanafranca598@gmail.com>
2025-08-08 11:59:27 -0300

    feat: refatora EmprestimoCardComponent para usar EmprestimoVencidoService para calcular datas e remove metodos que estavam no código e agora fazem parte do service.

--- src/app/shared/components/emprestimo-card/emprestimo-card.component.ts
+++ src/app/shared/components/emprestimo-card/emprestimo-card.component.ts
@@ -1,8 +1,9 @@
 import { DatePipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, inject, Inject, Input } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { EmprestimoDialogComponent } from '../emprestimo-dialog/emprestimo-dialog.component';
+import { EmprestimoVencidoService } from '../../services/emprestimo-vencido.service';
 
 @Component({
   selector: 'app-emprestimo-card',
@@ -12,19 +13,20 @@ import { EmprestimoDialogComponent } from '../emprestimo-dialog/emprestimo-dialo
   styleUrl: './emprestimo-card.component.scss'
 })
 export class EmprestimoCardComponent {
+  emprestimoVencidoService = inject(EmprestimoVencidoService);
   @Input() nomeBook: string = "";
   @Input() dataInicialEmprestimo!: Date;
   dataFinal!: Date;
   diasRestantes!: number;
   diasRestantesPositive!: number;
-  taxa = 1.30;
+  taxa = this.emprestimoVencidoService.taxa;
   dividaEmprestimo!: number;
 
   constructor(public dialog: MatDialog) { }
   
   ngOnChanges(): void {
-    this.dataFinal = this.calcularDataDevolucao();
-    this.diasRestantes = this.calcularDiasRestantes();
+    this.dataFinal = this.emprestimoVencidoService.calcularDataDevolucao(this.dataInicialEmprestimo);
+    this.diasRestantes = this.emprestimoVencidoService.calcularDiasRestantes(this.dataInicialEmprestimo);
     
     // quando os dias passam de 7 ficam negativos então usei a função para deixalos positivos
     this.diasRestantesPositive = Math.abs(this.diasRestantes);
@@ -42,17 +44,5 @@ export class EmprestimoCardComponent {
     });
   }
 
-  calcularDataDevolucao(){
-    return new Date(this.dataInicialEmprestimo.getTime() + 7 * 24 * 60 * 60 * 1000);    
-  }
-
-  calcularDiasRestantes(){
-    const hoje = new Date();
-    const dataDevolucao = this.calcularDataDevolucao();
-    const diffMs = dataDevolucao.getTime() - hoje.getTime();
-    return Math.ceil(diffMs / (1000 * 60 * 60 * 24));
-  }
 
 }
-
-// data de devolução = dtInicial + 7 dias
