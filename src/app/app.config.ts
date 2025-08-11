598cc8dc9c0e028f79cc21997e67078c62d5d6f1
Luana Paes <luanafranca598@gmail.com>
2025-08-11 18:08:55 -0300

    feat: adiciona configuração de opções padrão para diálogos no appConfig para resolver a questão do "blocked aria-hidden..." que estava aparecendo ao abrir o dialog material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes), provideAnimations(),
+    
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: { autoFocus: 'dialog', restoreFocus: true }
+    }
+  ]
 };
