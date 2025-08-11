b9f3392c9daae5c11c11cc14d791662e1cb141c6
Luana Paes <luanafranca598@gmail.com>
2025-08-11 16:49:11 -0300

    feat: injeta EmprestimoVencidoService e atualiza lógica de cálculo de dias e taxas no LeitorPanelComponent

--- src/app/pages/adm-dashboard/components/leitores/leitor-panel/leitor-panel.component.ts
+++ src/app/pages/adm-dashboard/components/leitores/leitor-panel/leitor-panel.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, inject, Input, input, signal } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BorrowedBook } from '../../../../../shared/interfaces/livro-emprestado';
 import { PhonePipe } from '../../../../../shared/pipe/phone-pipe/phone.pipe';
 import { CommonModule, DatePipe } from '@angular/common';
 import { TitlePipe } from '../../../../../shared/pipe/title-transform/title.pipe';
+import { EmprestimoVencidoService } from '../../../../../shared/services/emprestimo-vencido.service';
 
 @Component({
   selector: 'app-leitor-panel',
   standalone: true,
-  imports: [MatExpansionModule, PhonePipe, DatePipe, TitlePipe, CommonModule],
+  imports: [MatExpansionModule, PhonePipe, DatePipe, CommonModule, TitlePipe],
   templateUrl: './leitor-panel.component.html',
   styleUrl: './leitor-panel.component.scss'
 })
@@ -19,13 +20,41 @@ export class LeitorPanelComponent {
   @Input() email: string = '';
   @Input() numero_contato: string = '';
   @Input() livros_emprestados: BorrowedBook[] = [];
-
+  
+  //utilizado para calcular os dias do emprestimo
+  emprestimoVencidoService = inject(EmprestimoVencidoService);
+  dataFinal: Date[] = [];
+  diasRestantes: number[] = [];
+  diasRestantesPositive: number[] = [];
+  valorTaxa: number[] = [];
+  
   bookArray: BorrowedBook[] = [];
-
   livroAtual = 0;
 
   ngOnInit(): void {
-    this.loadBooks()
+    this.loadBooks();
+
+    //guarda os valores nos arrays para permitir resalizar as operações de forma individual em cada obj
+    for(let i = 0; i < this.livros_emprestados.length; i++){
+      this.dataFinal[i] = this.emprestimoVencidoService.calcularDataDevolucao(
+        this.livros_emprestados[i].data_emprestimo
+      );
+
+      this.diasRestantes[i] = this.emprestimoVencidoService.calcularDiasRestantes(
+        this.livros_emprestados[i].data_emprestimo
+      );
+
+      //os dias restantes podem ser menores que zero, para melhorar a visualização no card
+      //eu uso o math.abs para deixalos positivo , ex: -12 = 12
+      let diasPositive = Math.abs(this.diasRestantes[i])
+      this.diasRestantesPositive.push(diasPositive);
+
+      let valorTaxaEmprstimo = this.emprestimoVencidoService.calcularTaxa(this.diasRestantesPositive[i]);
+      this.valorTaxa.push(valorTaxaEmprstimo);
+    }
+
+    console.log(this.diasRestantesPositive)
+
   }
 
   avancar() {
